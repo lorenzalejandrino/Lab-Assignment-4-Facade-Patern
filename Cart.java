9ed3eb9aa074bf8d646bec0a52f5681b9d4e8884
lorenzalejandrino <158862246+lorenzalejandrino@users.noreply.github.com>
2024-10-23 14:23:08 +0800

    Cart.java

--- Cart.java
+++ Cart.java
@@ -1,8 +1,8 @@
 public class Cart implements HotelService {
     private int noCarts = 0;
 
-    public Cart(int noOfCarts){
-        this.noCarts = noOfCarts;
+    public Cart(int noCarts){
+        this.noCarts = noCarts;
     }
 
     @Override
@@ -10,7 +10,7 @@ public class Cart implements HotelService {
         return requestCart(noCarts);
     }
 
-    public String requestCart(int noOfCarts){
-        return ("Cart/s requested on room number: " + noOfCarts);
+    public String requestCart(int noCarts){
+        return ("Cart/s requested on room number: " + noCarts);
     }
 }
